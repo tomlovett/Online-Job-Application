9ce5ba555653e84628a42526b9aa80d411455036
Tom Lovett <tomlovett6@gmail.com>
2016-02-23 11:17:43 -0700

    fixed several architecture issues, working on routing

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,17 @@
 angular.module('JobApp', [])
 
 angular.module('JobApp')
-	.controller('homeController', ['$scope', function($scope){
-		
+	.controller('homeController', ['$scope', '$http', function($scope, $http){
+		$scope.submitApplication = function() {
+			$http.post('/api/applicant', $scope.applicant)
+				.then(function(returnData) {
+					console.log(returnData)
+					$scope.applicant = {}
+				})
+		}
 	}]);
 
 angular.module('JobApp')
 	.controller('applicantController', ['$scope', function($scope){
-		
+		'boop'
 	}]);
--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app.get('/applicants', function(req, res){
 });
 
 // creates and applicant
-app.post('/applicant', function(req, res){
+app.post('/api/applicant', function(req, res){
+	console.log(req.body)
 	// Here is where you need to get the data
 	// from the post body and store it in the database
 	res.send('No funciona');
